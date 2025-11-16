b6a3e6ebc3b9f1a7c4efcd76cf8f81e81b17c0c3
Sébastien Loix <seb@Sebastiens-MacBook-Pro.local>
2025-11-16 17:58:03 +0700

    Clean up

--- documentation/src/theme/Footer/index.tsx
+++ documentation/src/theme/Footer/index.tsx
@@ -1,11 +1,5 @@
 import React, { type ReactNode } from "react";
 
-import { useThemeConfig } from "@docusaurus/theme-common";
-import FooterLinks from "@theme/Footer/Links";
-import FooterLogo from "@theme/Footer/Logo";
-import FooterCopyright from "@theme/Footer/Copyright";
-import FooterLayout from "@theme/Footer/Layout";
-
 function Footer(): ReactNode {
   return (
     <footer className="pageFooter bg-gray-100 py-6">
