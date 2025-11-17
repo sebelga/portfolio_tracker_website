ef026edb11b554a7edf54b3ae92a510128a1d2d1
Sébastien Loix <seb@Sebastiens-MacBook-Pro.local>
2025-11-17 12:03:58 +0700

    Add Google Tag Manager

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -52,6 +52,9 @@ const config: Config = {
         theme: {
           customCss: "./src/css/custom.css",
         },
+        googleTagManager: {
+          containerId: "GTM-NGRM269Z",
+        },
       } satisfies Preset.Options,
     ],
   ],
