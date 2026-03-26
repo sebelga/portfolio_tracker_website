33c66e796e33246f16372af7dfc6f1e11a7d6165
Sébastien Loix <seb@Mac.Dlink>
2026-03-26 09:37:18 +0100

    Update footer to not include links

--- documentation/src/theme/Footer/index.tsx
+++ documentation/src/theme/Footer/index.tsx
@@ -2,23 +2,18 @@ import React, { type ReactNode } from "react";
 
 function Footer(): ReactNode {
   return (
-    <footer className="pageFooter bg-gray-100 py-6">
-      <div className="mx-auto mt-6 text-center text-sm leading-relaxed text-gray-500">
-        <p className="mb-4">
-          <a href="/">Home</a> | <a href="/contact">Contact</a> |{" "}
-          <a href="/docs">Documentation</a>{" "}
-          <span className="hidden md:inline">|</span>{" "}
-          <br className="md:hidden" />
-          <a href="/terms-and-services">Terms and services</a> |{" "}
-          <a href="/privacy-policy">Privacy policy</a>
+    <footer className="footer footer-horizontal bg-base-200 text-base-content border-base-300 border-t px-10 py-4">
+      <aside className="grid-flow-col items-center">
+        <p>
+          &copy; {new Date().getFullYear()} TradeGist Add-on. All rights
+          reserved.
         </p>
-        <p className="mb-2">
-          &copy; 2026 TradeGist Add-on. All rights reserved.
-        </p>
-        <p className="text-xs text-gray-400">
+      </aside>
+      <nav className="md:place-self-center md:justify-self-end flex items-center gap-4">
+        <p className="text-xs opacity-50">
           Google Sheets™ is a trademark of Google LLC.
         </p>
-      </div>
+      </nav>
     </footer>
   );
 }
